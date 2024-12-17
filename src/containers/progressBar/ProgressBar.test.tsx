4c67c4347a0c32c16bc16841db209f1f5b065ebe
lyzboy <13478602+lyzboy@users.noreply.github.com>
2024-12-17 10:45:07 -0700

    #22 feature: implement ProgressBar component to render symbols based on questionsData prop

--- src/containers/progressBar/ProgressBar.test.tsx
+++ src/containers/progressBar/ProgressBar.test.tsx
@@ -13,4 +13,26 @@ describe("Progress Bar",()=> {
     const progressBar = screen.getByTestId("progressBar");
     expect(progressBar).toBeInTheDocument();
   });
+  it(`should contain correct amount of progress bar symbols based on questionsData prop`, () => {
+    const data = [
+      {
+        current: false,
+        correct: false, 
+        completed: false,
+      },
+      {
+        current: false,
+        correct: false, 
+        completed: false,
+      },      {
+        current: false,
+        correct: false, 
+        completed: false,
+      },
+    ];
+    const dataLength = data.length;
+    render(<ProgressBar questionsData={data} />); // Explicitly pass completed prop
+    const progressBar = screen.getByTestId("progressBar");
+    expect(progressBar.children.length).toBe(dataLength);
+  });
 });
--- src/containers/progressBar/ProgressBar.tsx
+++ src/containers/progressBar/ProgressBar.tsx
@@ -1,5 +1,31 @@
-const ProgressBar = ()=>{
-  return <div data-testid="progressBar"></div>;
+import ProgressBarSymbol from "../../components/progressBarSymbol/ProgressBarSymbol";
+
+
+interface QuestionData {
+  current: boolean;
+  correct: boolean; 
+  completed: boolean;
+}
+
+
+interface ProgressBarProps {
+  questionsData?: Array<QuestionData>;
+};
+
+const ProgressBar = ({ questionsData }: ProgressBarProps) => {
+  if (!questionsData) questionsData = [];
+  return (
+    <div data-testid="progressBar">
+      {questionsData.map((questionData, index) => ( 
+        <ProgressBarSymbol
+          key={index}
+          current={questionData.current}
+          correct={questionData.correct} 
+          completed={questionData.completed}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default ProgressBar;
